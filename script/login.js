44b732460601766a682beea310536d73919e7f63
abuHafsGit <muhammadabuhafz@gmail.com>
2026-03-08 13:55:57 +0600

    GitHub Issues Tracker login page functionality added

---
+++ script/login.js
@@ -0,0 +1,22 @@
+console.log("loging js")
+
+document.getElementById('login').addEventListener('click', () => {
+    //get number
+    const getUserName = document.getElementById('userName')
+    const userName = getUserName.value;
+
+    //get pss 
+    const getUserPassword = document.getElementById('userPassword')
+    const userPassword = getUserPassword.value;
+
+    console.log(userName, userPassword)
+
+    if (userName == 'admin' && userPassword == 'admin123') {
+        alert("login")
+        // window.location.replace('/home.html')
+        window.location.assign('/home.html')
+    } else {
+        alert('invaild pass and number')
+    }
+
+})
